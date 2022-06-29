6399874521ef6ca30fa17b631b7938bd1ce45b5c
Ilya Pasternak <pasterzoom@gmail.com>
2022-06-30 00:58:54 +0300

    adding functionality

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from 'react'
 import style from './Footer.module.scss'
-import styleContainer from '../common/styles/container.module.scss';
 
 export const Footer = () => {
     return (
         <div className={style.footerBlock}>
-            <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h2 className={style.title}>Ilya Pasternak</h2>
-                <div className={style.items}>
-                    <div className={style.item}/>
-                    <div className={style.item}/>
-                    <div className={style.item}/>
-                    <div className={style.item}/>
+            <div className={style.footerContainer}>
+                <div className={style.inscription}>
+                    <p>Ilya Pasternak 2021</p>
                 </div>
-                <div className={style.inscription}>2021 All rights reserved</div>
+                <div className={style.items}>
+                    <div className={style.item}><a href='https://www.linkedin.com/in/ilya-pasternak-274951243/' target="_blank" rel="noreferrer">Linkedin</a></div>
+                    <div className={style.item}><a href='https://github.com/PasterZOOM' target="_blank" rel="noreferrer">GitHub</a></div>
+                    <div className={style.item}><a href='mailto:pasterzoom@gmail.com' target="_blank" rel="noreferrer">Email</a></div>
+                </div>
             </div>
         </div>
     )
